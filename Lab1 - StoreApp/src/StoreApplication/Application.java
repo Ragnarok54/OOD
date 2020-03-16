12fe394de8e999b5615d73d8225adf16d9204543
Ragnarok54 <bogdanzanfir23@gmail.com>
2020-03-16 21:35:44 +0200

    Added main Application

---
+++ Lab1 - StoreApp/src/StoreApplication/Application.java
@@ -0,0 +1,17 @@
+package StoreApplication;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Store store = new Store();
+		
+		store.addProduct("RTX 2070", 2670, 5);
+		store.addProduct("RTX 2080 Super", 3899, 4);
+		store.addProduct("RTX 2080Ti", 6600, 2);
+
+		// TODO: continue with an menu;
+
+	}
+
+}
